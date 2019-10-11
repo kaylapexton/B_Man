d83982ed974512141b97e66ce9db20e9a0835779
B Man <bstocksharp@gmail.com>
2019-10-11 10:33:30 -0600

    You're welcome guys

---
+++ DataStructures/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataStructures.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //this is the stack place
+        public ActionResult Stack()
+        {
+
+
+            return View();
+        }
+
+        //this is the Queue place
+        public ActionResult Queue()
+        {
+
+
+            return View();
+        }
+
+        //this is the dictionary place
+        public ActionResult Dictionary()
+        {
+
+
+            return View();
+        }
+
+        //this will exit (hopefully)
+        public ActionResult Exit()
+        {
+
+            return View();
+        }
+    }
+}
